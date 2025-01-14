a3988630a792d9e9e7e51532ee3f7f9a6654810b
Jowi <157366219+JowiMesas@users.noreply.github.com>
2025-01-14 19:28:26 +0100

    coches

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const vehiculos = [
-    new Coche('Lamborghini','blanda', 50, 180),
-    new Coche('Ferrari', 'media', 60, 190),
-    new Moto('Yamaha', 'dura', 40, 140),
-    new Moto('Kawasaki', 'media', 45, 120),
-    new Coche('Aston Martin', 'blanda', 55, 170)
-]
-const participantes = [
-    new Participante('Fernando Alonso','Aston Martin',32, 57, 140, 400),
-    new Participante('Marc Marquez', 'Yamaha', 24, 40, 57, 340),
-    new Participante('Joel', 'Ferrari', 2, 4, 0, 16)
-]
 
 class Circuito {
 constructor(nombre,tiempo, longitud) {
@@ -31,7 +19,6 @@ class Moto extends Vehiculo {
     caida = false;
     constructor(modelo, traccion, minAvance, maxAvance) {
         super(modelo, traccion, minAvance, maxAvance);
-        this.caida = caida;
     }
     calcularAvance(tiempo) {
         let avance = Math.floor(Math.random() * (this.maxAvance - this.minAvance + 1)) + this.minAvance;
@@ -40,13 +27,13 @@ class Moto extends Vehiculo {
         } else if (this.traccion === 'mediana') {
             avance +=2
         }
-        seCae(tiempo){
-            const porcentajes = {
-                lluvioso: {dura: 30, media: 20, blanda: 5},
-                humedo: {dura: 20, media: 10, blanda: 5},
-                seco: {dura: 5,media: 5, blanda: 5}
-            };
-        }
+        // seCae(tiempo){
+        //     const porcentajes = {
+        //         lluvioso: {dura: 30, media: 20, blanda: 5},
+        //         humedo: {dura: 20, media: 10, blanda: 5},
+        //         seco: {dura: 5,media: 5, blanda: 5}
+        //     };
+        // }
     }
 }
 class Coche extends Vehiculo {
@@ -65,3 +52,21 @@ class Participante {
         this.fueraPodio = fueraPodio;
     }
 }
+const vehiculos = [
+    new Coche('Lamborghini','blanda', 50, 180),
+    new Coche('Ferrari', 'media', 60, 190),
+    new Moto('Yamaha', 'dura', 40, 140),
+    new Moto('Kawasaki', 'media', 45, 120),
+    new Coche('Aston Martin', 'blanda', 55, 170)
+]
+const participantes = [
+    new Participante('Fernando Alonso',vehiculos[4],32, 57, 140, 400),
+    new Participante('Marc Marquez', vehiculos[2], 24, 40, 57, 340),
+    new Participante('Joel', vehiculos[1], 2, 4, 0, 16)
+]
+vehiculos.forEach((vehiculo) => {
+    document.getElementById('lista-vehiculos').innerHTML +=`<option value="${vehiculo.modelo}">${vehiculo.modelo}</option>`;
+});
+participantes.forEach((participante) => {
+    document.getElementById('lista-participantes').innerHTML +=`<option value="${participante.nombre}"> ${participante.nombre}</option>`;
+})
